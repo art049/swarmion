bb656b1205fbb777fccb711600a8b1b1cfbf8a9c
StanHannebelle <stanhannebelle@hotmail.com>
2022-05-06 17:05:47 +0200

    --wip-- [skip ci]

--- packages/serverless-contracts/src/contracts/apiGateway/apiGatewayContractFunctional.ts
+++ packages/serverless-contracts/src/contracts/apiGateway/apiGatewayContractFunctional.ts
@@ -1,9 +1,12 @@
+/* eslint-disable max-lines */
+import { AxiosInstance, AxiosResponse } from 'axios';
 import { FromSchema, JSONSchema } from 'json-schema-to-ts';
 import isUndefined from 'lodash/isUndefined';
 import omitBy from 'lodash/omitBy';
 
 import { ConstrainedJSONSchema } from 'types/constrainedJSONSchema';
 import { HttpMethod } from 'types/http';
+import { fillPathTemplate } from 'utils';
 
 import { ApiGatewayLambdaConfigType } from './lambdaTrigger';
 import {
@@ -11,7 +14,9 @@ import {
   ApiGatewayLambdaCompleteTriggerType,
   ApiGatewayLambdaSimpleTriggerType,
   ApiGatewayTriggerKey,
+  DefinedProperties,
   InputSchemaType,
+  RequestParameters,
 } from './types';
 
 interface ApiGatewayContract<
@@ -74,23 +79,6 @@ const createApiGatewayContract = <
   contractType: 'apiGatewayContract',
   ...contractProps,
 });
-
-const myContract = createApiGatewayContract({
-  id: 'testContract',
-  path: '/users/{userId}',
-  method: 'GET',
-  integrationType: 'httpApi',
-  pathParametersSchema: undefined,
-  queryStringParametersSchema: undefined,
-  headersSchema: undefined,
-  bodySchema: { type: 'string' },
-  outputSchema: {
-    type: 'object',
-    properties: { hello: { type: 'string' } },
-    additionalProperties: false,
-    required: ['hello'],
-  },
-} as const);
 
 const getTrigger = <Contract extends ApiGatewayContract>(
   contract: Contract,
@@ -186,4 +174,140 @@ const handler: HandlerType<typeof myContract> = async () => {
   return { hello: '' };
 };
 
-console.log(handler);
+type PathParametersType<Contract extends ApiGatewayContract> =
+  Contract['pathParametersSchema'] extends ConstrainedJSONSchema
+    ? FromSchema<Contract['pathParametersSchema']>
+    : undefined;
+type QueryStringParametersType<Contract extends ApiGatewayContract> =
+  Contract['queryStringParametersSchema'] extends ConstrainedJSONSchema
+    ? FromSchema<Contract['queryStringParametersSchema']>
+    : undefined;
+type HeadersType<Contract extends ApiGatewayContract> =
+  Contract['headersSchema'] extends ConstrainedJSONSchema
+    ? FromSchema<Contract['headersSchema']>
+    : undefined;
+type BodyType<Contract extends ApiGatewayContract> =
+  Contract['bodySchema'] extends ConstrainedJSONSchema
+    ? FromSchema<Contract['bodySchema']>
+    : undefined;
+
+const getRequestParameters = <Contract extends ApiGatewayContract>(
+  contract: Contract,
+  requestArguments: DefinedProperties<{
+    pathParameters: PathParametersType<Contract>;
+    queryStringParameters: QueryStringParametersType<Contract>;
+    headers: HeadersType<Contract>;
+    body: BodyType<Contract>;
+  }>,
+): RequestParameters<BodyType<Contract>> => {
+  // TODO improve inner typing here
+  const { pathParameters, queryStringParameters, headers, body } =
+    requestArguments as {
+      pathParameters: Record<string, string>;
+      queryStringParameters: Record<string, string>;
+      headers: Record<string, string>;
+      body: BodyType<Contract>;
+    };
+
+  const path =
+    typeof pathParameters !== 'undefined'
+      ? fillPathTemplate(contract.path, pathParameters)
+      : contract.path;
+
+  return omitBy(
+    {
+      method: contract.method,
+      path,
+      body,
+      queryStringParameters,
+      headers,
+    },
+    isUndefined,
+  ) as unknown as RequestParameters<BodyType<Contract>>;
+};
+
+const getAxiosRequest = async <Contract extends ApiGatewayContract>(
+  contract: Contract,
+  axiosClient: AxiosInstance,
+  requestArguments: DefinedProperties<{
+    pathParameters: PathParametersType<Contract>;
+    queryStringParameters: QueryStringParametersType<Contract>;
+    headers: HeadersType<Contract>;
+    body: BodyType<Contract>;
+  }>,
+): Promise<AxiosResponse<OutputType<Contract>>> => {
+  const { path, method, queryStringParameters, body, headers } =
+    // @ts-ignore: Type instantiation is excessively deep and possibly infinite.
+    getRequestParameters(contract, requestArguments);
+
+  return await axiosClient.request({
+    method,
+    url: path,
+    headers,
+    data: body,
+    params: queryStringParameters,
+  });
+};
+
+const pathParametersSchema = {
+  type: 'object',
+  properties: { userId: { type: 'string' }, pageNumber: { type: 'string' } },
+  required: ['userId', 'pageNumber'],
+  additionalProperties: false,
+} as const;
+
+const queryStringParametersSchema = {
+  type: 'object',
+  properties: { testId: { type: 'string' } },
+  required: ['testId'],
+  additionalProperties: false,
+} as const;
+
+const headersSchema = {
+  type: 'object',
+  properties: { myHeader: { type: 'string' } },
+  required: ['myHeader'],
+} as const;
+
+const bodySchema = {
+  type: 'object',
+  properties: { foo: { type: 'string' } },
+  required: ['foo'],
+} as const;
+
+const outputSchema = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    name: { type: 'string' },
+  },
+  required: ['id', 'name'],
+} as const;
+
+const myContract = createApiGatewayContract({
+  id: 'testContract',
+  path: '/users/{userId}',
+  method: 'GET',
+  integrationType: 'httpApi',
+  pathParametersSchema,
+  queryStringParametersSchema,
+  headersSchema,
+  bodySchema,
+  outputSchema,
+} as const);
+
+const { method, path, headers, queryStringParameters, body } =
+  getRequestParameters(myContract, {
+    pathParameters: { userId: '123', pageNumber: '12' },
+    headers: { myHeader: '12' },
+    queryStringParameters: { testId: '155' },
+    body: { foo: 'bar' },
+  });
+
+// {
+//   method: 'GET',
+//   path: '/users/123',
+//   headers: { myHeader: '12' },
+//   queryStringParameters: { testId: '155' },
+//   body: { foo: 'bar' },
+// }
