1089d948a126a2d38b4ac0348b35441b9c259836
axelf <axelf@theodo.fr>
2022-07-01 17:58:45 +0200

    feat(orchestrator): use new getCompleteLambdaHandler

--- backend/orchestrator/functions/requestSyncDeployment/handler.ts
+++ backend/orchestrator/functions/requestSyncDeployment/handler.ts
@@ -1,22 +1,18 @@
 import { ulid } from 'ulid';
 
 import { requestSyncDeployment } from '@swarmion/orchestrator-contracts';
-import { getLambdaHandler } from '@swarmion/serverless-contracts';
-import { applyHttpMiddlewares } from '@swarmion/serverless-helpers';
+import { getCompleteLambdaHandler } from '@swarmion/serverless-contracts';
 
 import ServiceEventEntity from 'libs/dynamodb/models/serviceEvent';
 
-const handler = getLambdaHandler(requestSyncDeployment)(async event => {
-  const { serviceId, applicationId } = event.body;
+export const main = getCompleteLambdaHandler(requestSyncDeployment)(
+  async event => {
+    const { serviceId, applicationId } = event.body;
 
-  const eventId = ulid();
+    const eventId = ulid();
 
-  await ServiceEventEntity.put({ serviceId, applicationId, eventId });
+    await ServiceEventEntity.put({ serviceId, applicationId, eventId });
 
-  return { status: 'ACCEPTED', message: 'processing' };
-});
-
-export const main = applyHttpMiddlewares(handler, {
-  inputSchema: requestSyncDeployment.inputSchema,
-  outputSchema: requestSyncDeployment.outputSchema,
-});
+    return { status: 'ACCEPTED', message: 'processing' };
+  },
+);
